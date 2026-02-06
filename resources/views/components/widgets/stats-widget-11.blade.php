2ce785c8d5c349c4125d96027c9471fb06e9b431
ASaifaji <aby110605@gmail.com>
2026-02-06 16:23:05 +0700

    refactor: move dashboard content to pages and its card to widgets

---
+++ resources/views/components/widgets/stats-widget-11.blade.php
@@ -0,0 +1,26 @@
+<!--begin::Stats Widget 11-->
+<div class="card card-custom card-stretch card-stretch-half gutter-b">
+    <!--begin::Body-->
+    <div class="card-body p-0">
+        <div class="d-flex align-items-center justify-content-between card-spacer flex-grow-1">
+            <span class="symbol symbol-50 symbol-light-success mr-2">
+                <span class="symbol-label">
+                    <span class="svg-icon svg-icon-xl svg-icon-success">
+                        <!--begin::Svg Icon | path:assets/media/svg/icons/Layout/Layout-4-blocks.svg-->
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                            <path fill="#1bc5bd" d="M96 128C60.7 128 32 156.7 32 192L32 256C32 264.8 39.4 271.7 47.7 274.6C66.5 281.1 80 299 80 320C80 341 66.5 358.9 47.7 365.4C39.4 368.3 32 375.2 32 384L32 448C32 483.3 60.7 512 96 512L544 512C579.3 512 608 483.3 608 448L608 384C608 375.2 600.6 368.3 592.3 365.4C573.5 358.9 560 341 560 320C560 299 573.5 281.1 592.3 274.6C600.6 271.7 608 264.8 608 256L608 192C608 156.7 579.3 128 544 128L96 128zM448 400L448 240L192 240L192 400L448 400zM144 224C144 206.3 158.3 192 176 192L464 192C481.7 192 496 206.3 496 224L496 416C496 433.7 481.7 448 464 448L176 448C158.3 448 144 433.7 144 416L144 224z"/>
+                        </svg>
+                        <!--end::Svg Icon-->
+                    </span>
+                </span>
+            </span>
+            <div class="d-flex flex-column text-right">
+                <span class="text-dark-75 font-weight-bolder font-size-h3">750 Open Ticket</span>
+                <span class="text-muted font-weight-bold mt-2">Ticket Status</span>
+            </div>
+        </div>
+        <div id="kt_stats_widget_11_chart" class="card-rounded-bottom" data-color="success" style="height: 150px"></div>
+    </div>
+    <!--end::Body-->
+</div>
+<!--end::Stats Widget 11-->
---
+++ resources/views/components/widgets/stats-widget-12.blade.php
@@ -0,0 +1,26 @@
+<!--begin::Stats Widget 12-->
+<div class="card card-custom card-stretch card-stretch-half gutter-b">
+    <!--begin::Body-->
+    <div class="card-body p-0">
+        <div class="d-flex align-items-center justify-content-between card-spacer flex-grow-1">
+            <span class="symbol symbol-50 symbol-light-primary mr-2">
+                <span class="symbol-label">
+                    <span class="svg-icon svg-icon-xl svg-icon-primary">
+                        <!--begin::Svg Icon-->
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#3699FF">
+                            <path d="M551-523q5-8 7-17.5t2-19.5q0-33-23.5-56.5T480-640q-10 0-19.5 2t-17.5 7l108 108Zm203 205-60-62q12-32 19-66.5t7-69.5v-189l-240-90-146 55-62-62 208-78 320 120v244q0 51-11.5 101T754-318Zm38 262L662-186q-38 39-84.5 65.5T480-80q-139-35-229.5-159.5T160-516v-172L56-792l56-56 736 736-56 56ZM430-418Zm57-170Zm-7 424q35-11 67-31t59-47L488-360h-68l10-58-190-190v92q0 121 68 220t172 132Z"/>
+                        </svg>
+                        <!--end::Svg Icon-->
+                    </span>
+                </span>
+            </span>
+            <div class="d-flex flex-column text-right">
+                <span class="text-dark-75 font-weight-bolder font-size-h3">+6,5K XSS Case</span>
+                <span class="text-muted font-weight-bold mt-2">Vulnerabilities</span>
+            </div>
+        </div>
+        <div id="kt_stats_widget_12_chart" class="card-rounded-bottom" data-color="primary" style="height: 150px"></div>
+    </div>
+    <!--end::Body-->
+</div>
+<!--end::Stats Widget 12-->
