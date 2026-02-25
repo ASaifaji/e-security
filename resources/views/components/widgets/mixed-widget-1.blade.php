907a5fcd26b45a07041a7eca647809f6d360d534
ASaifaji <aby110605@gmail.com>
2026-02-25 10:31:23 +0700

    feat: change the card text and icon for widget 1 in dashboard

--- resources/views/components/widgets/mixed-widget-1.blade.php
+++ resources/views/components/widgets/mixed-widget-1.blade.php
@@ -7,56 +7,38 @@
     <!--end::Header-->
     <!--begin::Body-->
     <div class="card-body p-0 position-relative overflow-hidden">
-        <!--begin::Chart-->
-        <div id="kt_mixed_widget_1_chart" class="card-rounded-bottom bg-danger" style="height: 200px"></div>
-        <!--end::Chart-->
+        <div class="card-rounded-bottom bg-danger" style="height: 200px"></div>
         <!--begin::Stats-->
         <div class="card-spacer mt-n25">
             <!--begin::Row-->
             <div class="row m-0">
-                <div class="col bg-light-warning px-6 py-8 rounded-xl mr-7 mb-7">
-                    <span class="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
-                        <!--begin::Svg Icon-->
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
-                            <path fill="#ffa800" d="M96 128C60.7 128 32 156.7 32 192L32 256C32 264.8 39.4 271.7 47.7 274.6C66.5 281.1 80 299 80 320C80 341 66.5 358.9 47.7 365.4C39.4 368.3 32 375.2 32 384L32 448C32 483.3 60.7 512 96 512L544 512C579.3 512 608 483.3 608 448L608 384C608 375.2 600.6 368.3 592.3 365.4C573.5 358.9 560 341 560 320C560 299 573.5 281.1 592.3 274.6C600.6 271.7 608 264.8 608 256L608 192C608 156.7 579.3 128 544 128L96 128zM448 400L448 240L192 240L192 400L448 400zM144 224C144 206.3 158.3 192 176 192L464 192C481.7 192 496 206.3 496 224L496 416C496 433.7 481.7 448 464 448L176 448C158.3 448 144 433.7 144 416L144 224z"/>
-                        </svg>
-                        <!--end::Svg Icon-->
-                    </span>
-                    <a href="#" class="text-warning font-weight-bold font-size-h6">Open Tickets</a>
-                </div>
-                <div class="col bg-light-primary px-6 py-8 rounded-xl mb-7">
+                <div class="col bg-light-primary px-6 py-8 rounded-xl mr-7 mb-7">
                     <span class="svg-icon svg-icon-3x svg-icon-primary d-block my-2">
-                        <!--begin::Svg Icon -->
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#3699FF">
-                            <path d="M551-523q5-8 7-17.5t2-19.5q0-33-23.5-56.5T480-640q-10 0-19.5 2t-17.5 7l108 108Zm203 205-60-62q12-32 19-66.5t7-69.5v-189l-240-90-146 55-62-62 208-78 320 120v244q0 51-11.5 101T754-318Zm38 262L662-186q-38 39-84.5 65.5T480-80q-139-35-229.5-159.5T160-516v-172L56-792l56-56 736 736-56 56ZM430-418Zm57-170Zm-7 424q35-11 67-31t59-47L488-360h-68l10-58-190-190v92q0 121 68 220t172 132Z"/>
-                        </svg>
-                        <!--end::Svg Icon-->
+                        <x-icons.ticket-02 />
                     </span>
-                    <a href="#" class="text-primary font-weight-bold font-size-h6 mt-2">Critical Vulnerabilities</a>
+                    <a href="#" class="text-primary font-weight-bold font-size-h6">Open Tickets</a>
+                </div>
+                <div class="col bg-light-danger px-6 py-8 rounded-xl mb-7">
+                    <span class="svg-icon svg-icon-3x svg-icon-danger d-block my-2">
+                        <x-icons.warning-1-circle />
+                    </span>
+                    <a href="#" class="text-danger font-weight-bold font-size-h6 mt-2">High Priority Tickets</a>
                 </div>
             </div>
             <!--end::Row-->
             <!--begin::Row-->
             <div class="row m-0">
-                <div class="col bg-light-danger px-6 py-8 rounded-xl mr-7">
-                    <span class="svg-icon svg-icon-3x svg-icon-danger d-block my-2">
-                        <!--begin::Svg Icon-->
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#F64E60">
-                            <path d="M197.37-71.87q-35.39 0-60.45-25.05-25.05-25.06-25.05-60.45v-154.26q0-35.39 25.05-60.45 25.06-25.05 60.45-25.05h565.26q35.39 0 60.45 25.05 25.05 25.06 25.05 60.45v154.26q0 35.39-25.05 60.45-25.06 25.05-60.45 25.05H197.37Zm5.5-91h554.26v-143.26H202.87v143.26Zm57.17-294.26L50.91-575.91l245.23-40.86-66.42-240.04 203.34 144.03 122.61-216.55 40.96 245.26 240.02-66.43-144.11 203.3 159.18 90.07H677.28l-109.82-62.15 58.89-83.46-98.74 27.33-17.04-100.74-49.61 88.45-83.46-58.89 27.33 98.74-100.98 17.28 122.39 73.44h-166.2Zm230.79 0ZM480-234.5Z"/>
-                        </svg>
-                        <!--end::Svg Icon-->
+                <div class="col bg-light-warning px-6 py-8 rounded-xl mr-7">
+                    <span class="svg-icon svg-icon-3x svg-icon-warning d-block my-2">
+                        <x-icons.clipboard-pending />
                     </span>
-                    <a href="#" class="text-danger font-weight-bold font-size-h6 mt-2">SLA Breached</a>
+                    <a href="#" class="text-warning font-weight-bold font-size-h6 mt-2">Pending Tickets</a>
                 </div>
                 <div class="col bg-light-success px-6 py-8 rounded-xl">
                     <span class="svg-icon svg-icon-3x svg-icon-success d-block my-2">
-                        <!--begin::Svg Icon-->
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1BC5BD">
-                            <path d="M542.8-138.5Q484.3-197 484.3-280t58.5-141.5Q601.3-480 684.3-480t141.5 58.5Q884.3-363 884.3-280t-58.5 141.5Q767.3-80 684.3-80t-141.5-58.5Zm208.5-44.11 30.4-30.63-75.48-75.48v-113.19h-43.59v130.56l88.67 88.74Zm-548.43 70.74q-37.54 0-64.27-26.73-26.73-26.73-26.73-64.27v-554.26q0-37.54 26.73-64.27 26.73-26.73 64.27-26.73h157.91q12.44-35.72 45.94-58.46 33.5-22.74 73.28-22.74 41.2 0 74.37 22.74t45.85 58.46h156.91q37.54 0 64.27 26.73 26.73 26.73 26.73 64.27v250q-19.91-14.91-42.9-25.47-22.99-10.55-48.1-17.07v-207.46H678.8v123.83H281.2v-123.83h-78.33v554.26h212.72q7.48 25.11 18.75 48.46 11.27 23.34 27.14 42.54H202.87ZM508.5-772.93q11.5-11.5 11.5-28.5t-11.5-28.5q-11.5-11.5-28.5-11.5t-28.5 11.5q-11.5 11.5-11.5 28.5t11.5 28.5q11.5 11.5 28.5 11.5t28.5-11.5Z"/>
-                        </svg>
-                        <!--end::Svg Icon-->
+                        <x-icons.clipboard-check />
                     </span>
-                    <a href="#" class="text-success font-weight-bold font-size-h6 mt-2">Pending Verification</a>
+                    <a href="#" class="text-success font-weight-bold font-size-h6 mt-2">Resolved today</a>
                 </div>
             </div>
             <!--end::Row-->
